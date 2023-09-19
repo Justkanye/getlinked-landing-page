48a9cf114375e1cc4bc8703db8ac04d560f4a000
Justkanye <adekanyetimilehin2000@gmail.com>
2023-09-19 07:51:18 +0100

    initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        primary: "",
+        "primary-bg": "#150e28",
+        "903aff": "#903aff",
+        d434fe: "#d434fe",
+      },
+    },
+  },
+  plugins: [daisyui],
+  darkMode: "class",
+};
+export default config;
