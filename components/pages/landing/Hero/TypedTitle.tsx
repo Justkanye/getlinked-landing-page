3caacf0535f20a6911a28c60523b1cab08a660ad
Justkanye <adekanyetimilehin2000@gmail.com>
2023-09-21 07:45:26 +0100

    typewriter effect

---
+++ components/pages/landing/Hero/TypedTitle.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { useState } from "react";
+import ReactTyped from "react-typed";
+
+import InvertedCurvedUnderline from "@/components/icons/InvertedCurvedUnderline";
+
+const TypedTitle = () => {
+  const [showCursor, setShowCursor] = useState(true);
+
+  return (
+    <div className='flex justify-center md:justify-end'>
+      <div className='flex flex-col items-end mr-12 mt-8'>
+        <h2 className='font-clash-display font-bold text-2xl md:text-4xl italic'>
+          Igniting{" "}
+          <ReactTyped
+            typeSpeed={40}
+            showCursor={showCursor}
+            strings={["a Revolution in HR Innovation"]}
+            onComplete={() => setShowCursor(false)}
+          />
+        </h2>
+        <InvertedCurvedUnderline width={165} />
+      </div>
+    </div>
+  );
+};
+
+export default TypedTitle;
