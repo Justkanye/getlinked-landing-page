2c4a808424ede14d311b9eee840bf656f0b433d2
Justkanye <adekanyetimilehin2000@gmail.com>
2023-09-21 20:10:25 +0100

    rewards section

---
+++ components/pages/landing/Prices/PriceCard.tsx
@@ -0,0 +1,50 @@
+import { cn } from "@/lib/core/utils";
+import Image from "next/image";
+
+const PriceCard = ({ alt, img, price, pos }: Props) => {
+  const emphasize = pos === "1st";
+  return (
+    <div
+      className={cn(
+        "card border-primary border rounded-lg bg-[rgba(212, 52, 254, 0.12)] w-32 md:w-56 h-40 md:h-80 mt-10 md:mt-[90px] relative",
+        {
+          "mt-16 md:mt-36 -translate-y-[7%]": emphasize,
+        }
+      )}
+    >
+      <figure
+        className={cn("absolute z-[2] aspect-square self-center")}
+        style={{
+          transform: `translateY(-${emphasize ? 65 : 50}%)`,
+          width: `${emphasize ? 135 : 95}%`,
+        }}
+      >
+        <Image src={img} alt={alt} fill />
+      </figure>
+      <div className='card-body justify-end items-center'>
+        <div className='text-center'>
+          <p
+            className={cn("font-bold text-base md:text-4xl", {
+              "md:text-[40px]": emphasize,
+            })}
+          >
+            {pos}
+          </p>
+          <p className='font-semibold text-sm md:text-2xl'>Runner</p>
+          <h4 className='text-sm md:text-2xl font-bold text-primary'>
+            {price}
+          </h4>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PriceCard;
+
+type Props = {
+  pos: string;
+  price: string;
+  img: string;
+  alt: string;
+};
---
+++ components/pages/landing/Prices/index.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+
+import PriceCard from "./PriceCard";
+
+const Prices = () => {
+  return (
+    <section className='py-16 px-20 border-border border-t' id='overview'>
+      <div className='grid grid-cols-1 md:grid-cols-2'>
+        <div />
+        <div className='items-center max-md:flex flex-col'>
+          <h2 className='font-clash-display font-bold text-2xl md:text-3xl leading-none'>
+            Prices and
+            <br />
+            <span className='text-primary'>Rewards</span>
+          </h2>
+          <p className='text-sm mt-4 max-md:text-center'>
+            Highlight of the prizes or rewards for winners and for participants.
+          </p>
+        </div>
+      </div>
+      <div className='grid grid-cols-1 md:grid-cols-5 gap-2 items-center mt-10 md:mt-24'>
+        <div className='md:col-span-2'>
+          <Image
+            src='/assets/images/trophy.png'
+            alt='trophy'
+            width={548}
+            height={482}
+          />
+        </div>
+        <div className='flex gap-3 md:col-span-3 max-md:mt-20'>
+          <PriceCard
+            img='/assets/images/silver_medal.png'
+            pos='2nd'
+            alt='silver medal'
+            price='N300,000'
+          />
+          <PriceCard
+            img='/assets/images/gold_medal.png'
+            pos='1st'
+            alt='silver medal'
+            price='N400,000'
+          />
+          <PriceCard
+            img='/assets/images/bronze_medal.png'
+            pos='3rd'
+            alt='bronze medal'
+            price='N150,000'
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Prices;
