f5d6e4539bf282af5f9c1a823ec606b08060c05c
Justkanye <adekanyetimilehin2000@gmail.com>
2023-09-23 02:54:14 +0100

    contact form api request

--- lib/features/contact/api.ts
+++ lib/features/contact/api.ts
@@ -1,21 +1,37 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { FormikHelpers } from "formik";
+import Swal from "sweetalert2";
 
 import { ContactFormValues } from "./form";
 import { BACKEND_URL } from "@/lib/core/constants";
 
+const alert = Swal.mixin({
+  icon: "error",
+});
+
 export const handleContactRequest = (
   values: ContactFormValues,
-  helpers: FormikHelpers<ContactFormValues>
+  { setSubmitting }: FormikHelpers<ContactFormValues>
 ) => {
-  console.log(values);
-  axios.post(`${BACKEND_URL}/hackathon/contact-form`, values).then(
-    res => {
-      console.log(res);
-    },
-    e => {
-      console.log(e);
-      helpers.setErrors({ message: "Failed" });
-    }
-  );
+  if (!BACKEND_URL) {
+    alert.fire("No API url provided. Please notify us of this error.");
+    setSubmitting(false);
+    return;
+  }
+  axios
+    .post(`${BACKEND_URL}/hackathon/contact-form`, values)
+    .then(
+      () => {
+        Swal.fire({
+          title: "Your message was recieved successfully",
+          icon: "success",
+        });
+      },
+      (e: AxiosError) => {
+        alert.fire(
+          e?.message ?? "An error occured. Please notify us of this error."
+        );
+      }
+    )
+    .finally(() => setSubmitting(false));
 };
